48ba93a69e4d8414d6a0648920cceb50c3d9eb0f
Hulihan Applications <dave@hulihanapplications.com>
2011-09-01 16:59:43 -0600

    Update thorness

--- lib/recursive_replace.rb
+++ lib/recursive_replace.rb
@@ -5,33 +5,25 @@ require File.expand_path(File.join("..", "recursive_replace_support"), __FILE__)
 
 class RecursiveReplace < Thor
   include ::RecursiveReplaceSupport
-  
-  #method_options :dry_run => false, :aliases => "-d"
+  class_option :verbose, :desc => "verbose", :type => :boolean, :default => true
+
+
+  class_option :user, :type => :string, :desc => "User to apply changes to.", :banner => "USERNAME", :required => false, :aliases => "-u" # required string option
+  method_option :dry_run, :type => :boolean, :desc => "See what would be replaced.", :default => false, :aliases => "-d"
   map "-v" => :version
   map "--version" => :version
 
   
-  default_task :replace 
+  default_task :version 
   
-  #argument :original, :required => true
-  #argument :replacement, :required => true 
-  #argument :original, :required => true, :banner => "original", :for => :replace 
-  #argument :replacement, :required => true, :banner => "replacement", :for => :replace
-  #argument :path, :optional => true, :banner => "path", :for => :replace
-  method_option :verbose => true
-  desc "replace", "Replace first string with second string. Path is optional"   
-  def replace(original, replacement, path)
-    if original.nil? || replacement.nil? #ARGV.length < 2
-      puts "Usage: recursive_replace [original] [replacement] [file or dir]"
-      exit 0 
-    end    
-    
+  desc "replace original replacement [path]", "Replace first string with second string. Path is optional"   
+  def replace(original, replacement, path = ".")    
     puts "Replacing #{original} with #{replacement} in #{path.nil? ? Dir.pwd : path}"        
   end
   
-  method_option :verbose => false
   desc "version", "get the current version"
   def version
+    puts options[:user] if options[:user]
      puts File.read(File.expand_path(File.join("..", "..", "VERSION"), __FILE__))
   end    
 end
